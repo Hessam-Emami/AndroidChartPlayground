b3000331f04576d2cd66d7b0370f20c2847d7613
Hessam Emami <hesam.e@fanap.plus>
2019-12-30 11:37:18 +0330

    Initial commit

---
+++ app/src/main/java/com/emami/charttest/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.emami.charttest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
+
+object ChartDataProvider {
+    fun generateDummyData() = List(100) { Random.nextInt(1, 100) }
+}
