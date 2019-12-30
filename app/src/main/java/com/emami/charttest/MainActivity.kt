e86c37cbef866e6d3e13de664b5e4b44c0eb1407
Hessam Emami <hesam.e@fanap.plus>
2019-12-30 13:19:19 +0330

    Created functions for x and y axis setup

--- app/src/main/java/com/emami/charttest/MainActivity.kt
+++ app/src/main/java/com/emami/charttest/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.emami.charttest
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.core.content.ContextCompat
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.charts.Chart
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.components.YAxis
 import com.github.mikephil.charting.data.*
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
@@ -16,24 +22,58 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        setupCharInteractions(barChart)
+        setupXAxis(barChart.xAxis, ContextCompat.getColor(this, R.color.gray))
+        setupYAxis(barChart.axisLeft, ContextCompat.getColor(this, R.color.gray))
         refreshButton.setOnClickListener {
             barChart.data = provideChartData()
             barChart.invalidate()
         }
+        refreshButton.performClick()
     }
-
 }
 
+private fun setupXAxis(xAxis: XAxis, gridColor: Int) {
+    xAxis.apply {
+        isEnabled = true
+        setDrawGridLines(false)
+        setAvoidFirstLastClipping(true)
+        axisLineColor = gridColor
+        enableAxisLineDashedLine(1.0f, 1.5f, 0f)
+        position = XAxis.XAxisPosition.BOTTOM
+    }
+}
+
+private fun setupYAxis(yAxis: YAxis, gridColor: Int) {
+    yAxis.apply {
+        axisLineColor = gridColor
+        enableAxisLineDashedLine(1.0f, 1f, 0f)
+        spaceBottom = 10f
+    }
+}
+
+private fun setupCharInteractions(barChart: BarChart) {
+    barChart.apply {
+        setScaleEnabled(false)
+        setTouchEnabled(false)
+        isDragEnabled = false
+        setDrawBorders(false)
+    }
+}
+
+
 private fun provideChartData(): BarData {
+    //Entry represents each point
     val entryList = ChartDataProvider.dummyDataForX.mapIndexed { index, xValue ->
         BarEntry(xValue.toFloat(), ChartDataProvider.generateDummyDataForY()[index].toFloat())
     }
+    //DataSet knows which items points belong together
     val dataSet = BarDataSet(entryList, "Label")
+    //Data holds all data needed for chart
     return BarData(dataSet)
 }
 
 object ChartDataProvider {
-    val dummyDataForX = (0..19).toList()
-    fun generateDummyDataForY() = List(20) { Random.nextInt(1, 100) }
+    val dummyDataForX = (0..6).toList()
+    fun generateDummyDataForY() = List(7) { Random.nextInt(1, 100) }
 }
